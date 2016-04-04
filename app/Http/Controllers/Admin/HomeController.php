c2191516262992c22ab4ab5c61149528c0293b1a
yohanes1989 <yohanes1989@hotmail.com>
2016-04-04 13:15:35 +0700

    create admins

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -3,11 +3,25 @@
 namespace GoProp\Http\Controllers\Admin;
 
 use GoProp\Http\Controllers\Controller;
+use GoProp\Models\User;
 
 class HomeController extends Controller
 {
     public function dashboard()
     {
+        for($i = 1; $i <= 3; $i += 1){
+            $user = User::where('email', 'admin'.$i.'@goprop.co.id')->first();
+            if(!$user){
+                $createdUser = User::create([
+                    'email' => 'admin'.$i.'@goprop.co.id',
+                    'username' => 'admin'.$i,
+                    'password' => bcrypt('goprop123')
+                ]);
+
+                $createdUser->assignRole('administrator');
+            }
+        }
+
         return view('admin.dashboard');
     }
 }
