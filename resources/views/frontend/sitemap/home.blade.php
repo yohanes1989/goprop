953f71e3aac6a12548e7528f2ca9358fdc7a4d93
yohanes1989 <yohanes1989@hotmail.com>
2016-07-20 23:29:46 +0700

    update seo

--- resources/views/frontend/sitemap/home.blade.php
+++ resources/views/frontend/sitemap/home.blade.php
@@ -3,14 +3,18 @@
 <sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     <sitemap>
         <loc>{{ route('frontend.sitemap.pages') }}</loc>
+        <changefreq>monthly</changefreq>
     </sitemap>
     <sitemap>
         <loc>{{ route('frontend.sitemap.properties') }}</loc>
+        <changefreq>daily</changefreq>
     </sitemap>
     <sitemap>
         <loc>{{ route('frontend.sitemap.resources') }}</loc>
+        <changefreq>weekly</changefreq>
     </sitemap>
     <sitemap>
         <loc>{{ route('frontend.sitemap.resource_categories') }}</loc>
+        <changefreq>monthly</changefreq>
     </sitemap>
 </sitemapindex>
